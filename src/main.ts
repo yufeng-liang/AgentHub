d8d994f3b531f55c699742d4dcb00ac22ad90c3c
yufengliang <1577865546@qq.com>
2026-09-21 23:36:07 +0800

    chore: 删掉 main.ts 里 Element Plus 全量注册残留的 zhCn 引用

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
-import zhCn from "element-plus/es/locale/lang/zh-cn";
 // ElMessageBox 是显式 import 的服务式组件（Sidebar.vue:7 等三处），插件不接管它的样式
 import "element-plus/es/components/message-box/style/css";
 // dark css-vars 绑定 html.dark，须在 element.css 之前引入，让项目主题变量赢
